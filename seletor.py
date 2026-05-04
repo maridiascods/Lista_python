d7483d14046817c9051f24778161706a6fb60a0f
maridiascods <polligodoydias@gmail.com>
2026-05-04 07:51:49 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#seletor de caracteres --
+letra = input("Digite uma letra.").upper()
+if letra == 'A':
+   print("Você Selecionou a Opção A")
+elif letra == 'B':
+  print("Você Selecionou a Opção B")
+else:
+  ("Essa Opção não esta disponivel")
