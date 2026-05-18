e0f00d361a34cdd258abae0b22cf94637e9e6ba8
maridiascods <polligodoydias@gmail.com>
2026-05-18 10:52:42 +0000

    v1

--- tabuada.py
+++ tabuada.py
@@ -1,1 +1,10 @@
+#-- tabuada automatica--
+print("Bem vindo a tabuada automatica")
+numero = int(input("Tabuada de qual numero? "))
+print(f"---- Tabuada do numero {numero} ----")
 
+for i in range(1,11):
+    resultado = numero * i
+    print(f" {numero} x {i} = {resultado}")
+    print("-----------------------------")
+print(":)")
