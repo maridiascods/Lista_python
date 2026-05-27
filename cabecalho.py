b4c5e9111c05bafe0e4a09a414b46968509729d3
maridiascods <polligodoydias@gmail.com>
2026-05-27 13:15:38 +0000

    v2

--- cabecalho.py
+++ cabecalho.py
@@ -1,8 +1,10 @@
+# definido chamando a função--
 def exibir_cabecalho():
     print('=' * 30)
     print('SISTEMA DE GESTÃO ESCOLAR')
     print('versão 1.0 - Desenvolvimento no Codespaces')
     print('=' * 30)
 
+#chamando a função--
     exibir_cabecalho()
     print('Menu prinicipal:')
